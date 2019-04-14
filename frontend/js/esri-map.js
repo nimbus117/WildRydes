7fe742fea7fc6b21330d8c6b8701537bcdec1931
nimbus117 <mail@nimbus117.co.uk>
2019-04-14 18:34:31 +0100

    changed center point in esri-map.js to used coords for edinburgh

--- frontend/js/esri-map.js
+++ frontend/js/esri-map.js
@@ -23,7 +23,7 @@ WildRydes.map = WildRydes.map || {};
         var map = new Map({ basemap: 'gray-vector' });
 
         var view = new MapView({
-            center: [-122.31, 47.60],
+            center: [-3.188267, 55.953251],
             container: 'map',
             map: map,
             zoom: 12
