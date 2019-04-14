bf79ff52a9ea42ee9429e4322149a94c929eadd9
nimbus117 <mail@nimbus117.co.uk>
2019-04-14 18:54:48 +0100

    changed esri basemap to streets-navigation-vector

--- frontend/js/esri-map.js
+++ frontend/js/esri-map.js
@@ -20,7 +20,7 @@ WildRydes.map = WildRydes.map || {};
     ) {
         var wrMap = WildRydes.map;
 
-        var map = new Map({ basemap: 'gray-vector' });
+        var map = new Map({ basemap: 'streets-navigation-vector' });
 
         var view = new MapView({
             center: [-3.188267, 55.953251],
